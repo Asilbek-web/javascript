2b48b037d6105d2fec6a0b554d7222dee1edccb1
Asilbek Boysoatov <boysoatovasilbek@gmail.com>
2022-11-07 15:40:43 +0500

    javascript
    hello

---
+++ main.js
@@ -0,0 +1,797 @@
+// Java Script asoslari if va else tushunchasi
+
+// if va else ishlash yosh misolida
+
+
+// let yosh = prompt('yoshingizni kiriting');
+
+
+
+
+//          if(yosh <= 2){
+//              document.write('Home ')
+//          }
+//          else if(yosh >= 3 && yosh <= 6 ){
+//              document.write("kindegarden ")
+//          }
+//          else if(yosh >= 7 && yosh <= 16 ){
+//              document.write("school ")
+//          }
+//          else if(yosh >= 18 && yosh <= 21 ){
+//              document.write("University ")
+//          }
+//          else if(yosh >= 22 && yosh <= 56 ){
+//              document.write("work ")
+//          }
+//          else if ( yosh >= 56 && yosh <= 80){
+//              document.write('retirn ')
+//          }else {
+//             document.write("siz endi dam oling ") 
+//          }
+            
+         
+
+
+
+
+
+        // let lang = prompt('enter your language');
+
+        // if( lang == "uz"){
+
+        //     let alfabeht = prompt('Enter your alfabeht');
+
+        //     if (alfabeht == 'latin'){
+        //         document.write('salom')
+        //     }else{
+        //         document.write('nun')
+        //     }
+        // }
+        // else if( lang == "jp"){
+        //     document.write('konichiva');
+        // }
+        // else if ( lang == "en"){
+        //     document.write('Hello')
+        // }
+
+
+
+
+
+        // let age = prompt('yoshingizni kiriting!')
+
+        // if ( age >= 18 && age <= 21 ){
+        //     document.write('siz hujjat topshira olasiz ')
+        // }else{
+        //     document.write('afsuski yoshingiz togri kelmadi ');
+        // }
+
+        // let lang = prompt('qaysi tilni bilasiz?');
+
+        // if(lang == "english" || lang == "japan" || lang == "uzbek"){
+        //     document.write("siz o'tdingiz")
+        // }else{
+        //     document.write('otmadingiz')
+        // }
+
+       
+
+
+
+         // let dangasa = prompt('siz dangasamisiz?') 
+
+        // if ( dangasa != "ha" ){
+        //     document.write(' siz qabul qilindingiz')
+        // }else{
+        //     document.write('dangasalar bizga kerak emas uzr')
+        // }
+
+
+
+
+
+
+        // let savol1 = prompt("salom so'zi yapon tilida qanday bo'ladi?");
+
+
+        // if (savol1 == "konichiva"){
+
+        //     let savol2 = prompt("orange so'zining ma'nosi nima")
+
+        //     if ( savol2 == "binafsha" || savol2 == "olov rang"){
+        //         document.write("a'lo")
+        //     }else{
+        //         document.write("yaxshi")
+        //     }
+
+        // }else if (savol1 != "konichiva") {
+        //     document.write("noto'g'ri")
+        // }
+
+        // function evenOrOddDec(number){
+        //     if (number % 2 === 0){
+        //         console.log( ` ${number} is even`);
+        //     }else {
+        //         console.log (`${number} is odd`);
+        //     }
+        // }
+        // evenOrOddDec(3);
+
+
+        // function evenOrOddExp(number){
+        //     if (number % 2 === 0){
+        //         console.log( ` ${number} is even`);
+        //     }else {
+        //         console.log (`${number} is odd`);
+        //     }
+        // }
+        // evenOrOddExp(4);
+
+        // const evenOrOddArrow = number =>{
+        //     if (number % 2 === 0){
+        //         console.log( ` ${number} is even`);
+        //     }else {
+        //         console.log (`${number} is odd`);
+        //     }
+        // }
+        // evenOrOddArrow(5);
+
+
+        // function maxDec ( number1, number2){
+        //     if (number1 >= number2) {
+        //         return  number1;
+        //     }
+        //     return number2;
+        // }
+
+        // console.log(maxDec(6,5))
+
+        //   function maxExp ( number1, number2,){
+//             if (number1 <= number2) {
+//                 return  number1;
+//             }
+//             return number2;
+//         }
+
+//         console.log(maxExp(9,8))
+
+
+      
+
+
+//         let a = 5;
+//         let b = 10;
+//         let c = 3;
+//         son = a * b * c;
+// console.log(son);
+        
+// let a = 10;
+
+// console.log(a++)
+// console.log(a)
+// console.log(a--)
+// console.log(a)
+// let b = 10;
+// b= 15;
+// console.log(b=30);
+// console.log(++b);
+
+// let a = 0;
+
+// let b = 3;
+// let c = 2
+
+// if(a > 0){
+//         console.log(1 + a);
+// }else if (a==0){
+//        console.log(a + 10);
+// }else{
+//         console.log(a - 2);
+// }
+
+
+
+// 5 savol
+
+// let x = 10;
+// let y = -1;
+// let z = -1;
+// let m = 0;
+// let p = 0;
+
+// if(x > 0 ){
+//         p++;    
+// }else if(x < 0){
+//         m++;
+// }
+        
+
+// if(y > 0){
+//         p++;
+// }else if(y < 0){
+//         m++;
+// }
+// if(z > 0){
+//         p++;
+// }else if(z < 0){
+//         m++;
+// }
+        
+
+
+// console.log("Musbat sonlar :"+p+" ta Manfiy sonlar :"+m+" ta")
+
+// 6 savol
+
+
+// let a = 5;
+// let b = 8;
+
+// if( a > b ){
+//         console.log(a)
+// }else{
+//         console.log(b)
+// }
+
+
+// 7 savol
+
+
+// let a = 5;
+// let b = 2;
+
+
+// let n = 1;
+// let c = 2;
+
+
+// if (a < b ){
+//         console.log(n);
+// }else{
+//         console.log(c)
+// }
+
+// 8 savol
+
+// let c = 56;
+// let b = 10;
+
+// if(c > b){
+//         console.log(c);
+//         console.log(b);
+// }else{
+//         console.log(b);
+//         console.log(c);
+// }
+
+// 9 savol
+
+// let a = 5;
+// let b = 10;
+
+// if(a < b){
+//         console.log(a)
+//         console.log(b)
+// }else{
+//         console.log(a)
+//         console.log(b)
+// }
+
+// let a = 25;
+// let b = 30;
+// let c = 15;
+
+// if (a > b){
+//         if(b > c){
+//                 console.log(a);
+//         }else{
+//                 console.log(b);
+//         }
+// }else{
+//         if(a > c){
+//                 console.log(a);
+//         }else{
+//                 console.log(c);
+//         }
+// }
+
+
+
+
+
+
+
+
+
+// let a = 4;
+// let b = 9;
+// let c = 2;
+
+// if (a <= b && a <= c){
+//         console.log(a);
+// }else if(b <= c && b <= a){
+//         console.log(b);
+// }else{
+//         console.log(c);
+// }
+
+
+
+// let a = 60;
+// let b = 50;
+// let c = 30;
+
+// if (a < b && a < c){
+//         console.log(a);
+// }else if(b < c && b < a){
+//         console.log(b);
+// }else{
+//         console.log(c);
+// }
+
+
+// let a = 8;
+// let b = 87;
+// let c = 86;
+
+// if (a >= b && a >= c){
+//         console.log(a);
+// }else if(b >= c && b >= a){
+//         console.log(b);
+// }else{
+//         console.log(c);
+// }
+
+// let a = 8;
+// let b = 1;
+// let c = -6;
+
+// if (a >= b && a >= c){
+//         console.log(a);
+// }else if(b >= c && b >= a){
+//         console.log(b);
+// }else{
+//         console.log(c);
+// }
+
+// 
+
+
+
+
+
+
+
+// let a = prompt("yoshingizni kiriting..")
+// let age = 18;
+// let toshi = 2000;
+// if (a <= age){
+//         document.write("siz hali yosh ekansiz keyingi yil keling");
+
+// }else{
+//         document.write(" xush kelibsiz");
+// }
+
+
+// let a = 5;
+// let b = 6;
+// let n = 0;
+
+
+// if ((a > b)|| (b > a)){
+//         console.log(a);
+//         console.log(b);
+// }
+
+// const fruits = [2, 1, 3,99,22,11];
+// fruits.sort();
+// console.log(fruits);
+// const numbers = [25,90 ,21,1];
+// console.log(Math.min.apply(null,numbers));
+
+
+
+// let a = 5; 
+// let b = 6;
+
+// let rem;
+
+
+// let a = prompt("A sonni kiriting");
+// let b = prompt("B sonni kiriting");
+// let rem;
+// if (a > b) {
+//         rem = a; // a ning qiymatini saqlab qolamiz
+//         a = b;
+//         console.log("a kichik " + a);
+//         b = rem; // keyinchalik b ga o'tkazib beramiz
+//         console.log("b katta " + b);
+// } else{ 
+//         console.log("a kichik " + a);
+//         console.log("b katta " + b);
+// }
+
+// let a = Number(prompt("A sonni kiriting"));
+// let b = Number(prompt("B sonni kiriting"));
+// if (a != b) {
+//         console.log(a + b);
+// } else {
+//         a = 0;
+//         b = 0;
+//         console.log(a,b);
+// }
+
+// let a = 99;
+// let b = 1000;
+// let c = 1236;
+
+// if ((a > b && b > c) || (a > c && c > b)){
+//         console.log(a);
+// }else if((b > a && a > c) || (b > c && c > a)){
+//         console.log(b);
+// }else{
+//         console.log(c);
+// }
+
+// let a = 99;
+// let b = 10;
+// let c = 1236;
+
+// if ((a < b && b < c) || (a < c && c < b)){
+//         console.log(a);
+// }else if((b < a && a < c) || (b < c && c < a)){
+//         console.log(b);
+// }else{
+//         console.log(c);
+// }
+
+
+
+
+
+
+
+
+
+
+
+// let a = (prompt("son kiriting"));
+
+
+// if(a % 2 == 0 && a >= 0){
+//         console.log('musbat juft son')
+// } else if(a % 2 == 0 && a < 0){
+//         console.log('manfiy juft son')
+// }else if(a % 2 && a < 0){
+//         console.log('manfiy toq son')
+// }
+// else{
+//         console.log(' musbat toq son')
+// }
+
+// let a = myFunction(4,5);
+// document.write(myFunction);
+// function myFunction(a , b){
+//         return a * b
+        
+// }
+
+
+// function displayDate(){
+//         document.getElementById("demo").innerHTML = Date()
+// }
+
+
+
+
+// Bugun darsimizda ushbu tegralni o'tdildi: while -- sonalarni takrorlanishida ishlatiladi
+// Bugun darsimizda ushbu tegralni o'tdildi: for  -- 
+// sonalarni takrorlanishida ishlatiladi: while dan farqi forda shartlarni bitta qatorda yozishimiz mukin va qulayroq
+// 
+// 
+// 
+
+
+
+// while( a <= 100 ){
+//                 document.write(a + ", ")
+//         a++;
+        
+// }
+
+// for(let a = 10; a <= 100; a += 5){
+//         document.write(a + ", ")
+// }   \
+
+
+
+
+
+
+// function kattasiniTop(a, b ,c){
+// if (a > b && a > c ){
+//         document.write(a);
+// }else if (b > a && b > c){
+//         document.write(b)
+// }else{
+//         document.write(c)
+// }
+// }
+
+// kattasiniTop(40, 99,13);
+
+
+// function kattasiniTop(a, b){
+//         if (a > b ){
+//                 return a;
+//         }else{
+//                 return b
+//         }
+// }
+//         document.write(kattasiniTop(7,5) + (kattasiniTop(6,7)));
+
+
+
+
+
+
+
+
+// let a = prompt("son yozing");
+
+// if (a % 2 == 0){
+//         document.write("juft son")
+// }else{
+//         document.write("toq son")
+// }
+
+// let son = prompt("Son kiriting..");
+
+// tubSonniTop(son);
+
+// function tubSonniTop(n){
+//         let tub = false;
+
+// for (let a = 2; a < n; a++){
+//         if (n % a == 0){
+//                 tub = true;
+//         }
+// }
+// if(tub){
+//         document.write("murakkab son")
+// }else{
+//         document.write("tub son")
+// }
+// }
+
+
+
+
+// const findMiddleNumber = (a, b, c) => {
+//     if (a > b && a > c) {
+//         if (b > c) {
+//             return b;
+//         } else {
+//             return c;
+//         }
+//     } else if (b > a && b > c) {
+//         if (a > c) {
+//             return a;
+//         } else {
+//             return c;
+//         }
+//     } else if (c > a && c > b) {
+//         if (b > a) {
+//             return b;
+//         } else {
+//             return a;
+//         }
+//     }
+// }
+// console.log(findMiddleNumber(4, 6, 8));
+
+
+
+// let a = 5;
+// let b = 3;
+// let c = 7;
+
+// if ((b < a && a < c) || (c < a && a < b)){
+//         console.log(a);
+
+// }else if ((a < b && b < c) || (c < b && b < a)){
+//         console.log(b);
+
+// }else if ((a < c && c < b) || (b < c && c < a)){
+//         console.log(c);
+
+// }
+
+
+// let a = 9
+// let b = 8
+// let c = 10
+// if(a > b){
+//     if(b > c){
+//         console.log(b);
+//     }else if(c > a){
+//         console.log(a)
+//     }else{
+//         console.log(c);
+//     }
+// }else{
+//     if(a>c){
+//         console.log(a);
+//     }else if(c > b){
+//         console.log(b)
+//     }else{
+//         console.log(c)
+//     }
+// }
+
+
+// let a = 276;
+// let b = (a % 10);
+// let c = ((a - (a - (a % 100))) - b) / 10;
+// let n = (a - (a % 100)) / 100;
+
+// console.log(n);
+// console.log(c);
+// console.log(b);
+
+
+// linear search va binry search 
+// class Node{
+//         constructor(data){
+//                 this.data = data;
+//                 this.next = null;
+//         }
+// }
+// class LinkedList{
+//         constructor(){
+//                 this.head = null;
+//                 this.size = 0;
+//         }
+//         // add bu listning oxiriga yangi node qo'shishi;
+//         add(data){
+//                 var node = new Node(data);
+//                 var current;
+
+//                 if (this.head == null){
+//                         this.head = node;
+//                 } else {
+//                         current = this.head;
+//                         while(current.next){
+//                                 current = current.next;
+//                         }
+//                         current.next = node;
+//                 }
+//                 this.size++;
+//         }
+//         getFirst(){
+//                 return this.head;
+//         }
+
+//         getLast(){
+//                 let lastNode = this.head;
+//                 if(lastNode){
+//                         while(lastNode.next){
+//                                 lastNode = lastNode.next
+//                         }
+//                 }
+//                 return lastNode;
+//         }
+
+//         printList(){
+//                 var curr = this.head;
+//                 var str = "";
+//                 while (curr){
+//                         str += curr.data + "";
+//                         curr = curr.next;
+//                 }
+//                 console.log(str);
+//         }
+// }
+
+
+// var list = new LinkedList();
+// list.add(6);
+// list.printList();
+
+// list.add(10);
+// list.add(20);
+// list.add(30);
+// list.add(40);
+// list.add(50);
+// list.add(60);
+// list.printList();
+
+// console.log(list.getFirst())
+// console.log(list.getLast());
+
+
+
+// class Pet{
+//         constructor(name){
+//                 this.name = name;
+
+//         }
+//         eat(food){
+//                 return this.name + " is eating " + food + ".<br>";
+//         }
+// }
+
+// class Dog extends Pet{
+
+// }
+// class Cat extends Pet{
+        
+// }
+// let  dog1 = new Dog("Sharik");
+// let  cat1 = new Cat("momiq");
+
+// document.write( dog1.eat("meal") );
+// document.write( dog1.eat("bone") );
+// document.write( dog1.eat("meat") );
+
+// document.write("<hr>")
+
+// document.write( cat1.eat("meal") );
+// document.write( cat1.eat("bone") );
+// document.write( cat1.eat("meat") );
+
+
+// class Car{
+//         constructor(name){
+//                 this.name = name;
+
+//         }
+        
+//         comfort(drive){
+//                    return this.name + " is " + drive + ". <br>";
+//         }
+// } 
+
+// class Bugatti extends Car{
+
+// }
+// class Gentera extends Car{
+        
+// }
+// let  bug1 = new Bugatti("Bugatti");
+// let  gm1 = new Gentera("Gentera");
+
+// document.write( bug1.comfort("is 400 km faster") );
+// document.write( bug1.comfort("is expensive") );
+// document.write( bug1.comfort("is sportcar") );
+
+// document.write("<hr>")
+
+// document.write( gm1.comfort(" comfort") );
+// document.write( gm1.comfort(" expensive") );
+// document.write( gm1.comfort(" poor quality") );
+
+
+// parent class 
+
+class Person{
+        constructor(name){
+                this.name = name;
+        }
+       
+        greet(){
+                document.write(`Hello ${this.name}`);
+        }
+}
+
+
+// inherting parent class
+
+class Student extends  Person {
+
+}
+
+let student1 = new Student ('Jack');
+student1.greet();
+
+
